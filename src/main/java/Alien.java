806fd844323158a39113e56630cf45bcab904e23
serena.peschiaroli <serena.peschiaroli@gmail.com>
2024-07-04 14:34:15 +0200

    Add: entity abstract class, human class, alien class

--- src/main/java/Alien.java
+++ src/main/java/Alien.java
@@ -1,2 +1,62 @@
-public class Alien {
+public class Alien extends Entity implements Food, TakeDamage {
+    private int mana;
+
+    public Alien(String name, int health, String abilities) {
+        super(name, health, abilities);
+        this.mana = 100; // Initial mana for the Alien
+    }
+
+    // Getter for mana
+    public int getMana() {
+        return mana;
+    }
+
+    // Setter for mana
+    public void setMana(int mana) {
+        this.mana = mana;
+    }
+
+    @Override
+    public void useAbility() {
+        if (mana >= 10) {
+            System.out.println(getName() + " uses an alien-specific ability: " + getAbilities());
+            setMana(getMana() - 10);
+            System.out.println("Mana remaining: " + getMana());
+        } else {
+            System.out.println(getName() + " does not have enough mana to use the ability.");
+        }
+    }
+
+    @Override
+    public void eatFood() {
+        System.out.println(getName() + " eats alien food to gain special effects.");
+        setMana(getMana() + 10);
+        System.out.println("Mana increased: " + getMana());
+    }
+
+    @Override
+    public void takeDamage(int damage) {
+        setHealth(getHealth() - damage);
+        System.out.println(getName() + " takes " + damage + " damage, health is now " + getHealth());
+    }
+
+    public void regenerateHealth() {
+        if (mana >= 10) {
+            System.out.println(getName() + " regenerates health.");
+            setHealth(getHealth() + 10);
+            setMana(getMana() - 10);
+            System.out.println("Health regenerated to: " + getHealth());
+            System.out.println("Mana remaining: " + getMana());
+        } else {
+            System.out.println(getName() + " does not have enough mana to regenerate health.");
+        }
+    }
+
+    public void mutate() {
+        System.out.println(getName() + " mutates into a different form.");
+        setHealth(getHealth() - 5);
+        setMana(getMana() + 15);
+        System.out.println("Health after mutation: " + getHealth());
+        System.out.println("Mana after mutation: " + getMana());
+    }
 }
--- src/main/java/Human.java
+++ src/main/java/Human.java
@@ -1,45 +1,65 @@
 public class Human extends Entity implements Food, TakeDamage {
 
     private boolean isWearingArmor;
-    public Human(String name, int health, String abilities) {
+    private int stamina;
 
+    public Human(String name, int health, String abilities) {
         super(name, health, abilities);
         this.isWearingArmor = false;
+        this.stamina = 100;
     }
 
+    public int getStamina() {
+        return stamina;
+    }
+
+    public void setStamina(int stamina) {
+        this.stamina = stamina;
+    }
 
     @Override
     public void useAbility() {
-        System.out.println(getName() + " uses a human-specific ability: " + getAbilities());
-
+        if (stamina >= 10) {
+            System.out.println(getName() + " uses a human-specific ability: " + getAbilities());
+            setStamina(getStamina() - 10);
+            System.out.println("New stamina: " + getStamina());
+        } else {
+            System.out.println(getName() + " does not have enough stamina to use the ability.");
+        }
     }
 
     @Override
     public void eatFood() {
-        System.out.println(STR."\{getName()} eats food to gain special effects.");
+        System.out.println(getName() + " eats food to gain special effects.");
         setHealth(getHealth() + 10);
         System.out.println("Health regained: " + getHealth());
-
+        setStamina(getStamina() + 5);
+        System.out.println("Stamina: " + getStamina());
     }
 
     @Override
     public void takeDamage(int damage) {
-        if(isWearingArmor){
+        if (isWearingArmor) {
             damage *= 0.9;
         }
         setHealth(getHealth() - damage);
+        setStamina(getStamina() + 5);
         System.out.println(getName() + " takes " + damage + " damage, health is now " + getHealth());
-
-
     }
 
     public void wearArmor() {
-        this.isWearingArmor = true;
-        System.out.println(getName() + " is now wearing armor.");
+        if (stamina >= 10) {
+            this.isWearingArmor = true;
+            setStamina(getStamina() - 10);
+            System.out.println(getName() + " is now wearing armor. Armor consumes stamina.");
+        } else {
+            System.out.println(getName() + " does not have enough stamina to wear armor.");
+        }
     }
 
     public void removeArmor() {
         this.isWearingArmor = false;
+        setStamina(getStamina() + 10);
         System.out.println(getName() + " has removed the armor.");
     }
 }
